54f1b19ae2b98a4c34854ddb4ca93d7346b1b5c8
Sagar Dulal <sagardulal100@gmail.com>
2021-12-16 23:49:04 +0545

    Update of the products routes added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,3 +38,27 @@ exports.getSingleProduct = async(req,res,next) =>{
         product
     })
 }
+
+// Update Products => /api/v1/product/:id
+
+exports.updateProduct = async(req,res)=>{
+    
+    let product = await Product.findById(req.params.id);
+    
+    if( !product){
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    product  = await Product.findByIdAndUpdate(req.params.id , req.body,{
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+    res.status(200).json({
+        success: true,
+        product
+
+    })
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,12 @@
 const express= require('express');
 const router = express.Router();
-const {getProducts, newProduct, getSingleProduct} = require('../controllers/productController')
+const {getProducts, newProduct, getSingleProduct ,updateProduct } = require('../controllers/productController')
 
-
-router.route('/products/new').post(newProduct)
+// Getting of the products
 router.route('/products').get(getProducts)
 router.route('/product/:id').get(getSingleProduct)
+
+// Posting new products as well as updating by the admin
+router.route('/admin/products/new').post(newProduct)
+router.route('/admin/product/:id').put(updateProduct)
 module.exports = router
