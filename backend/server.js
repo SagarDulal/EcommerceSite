d53f1c1d05fdc14ab113746524f712b2665fcddf
Sagar Dulal <sagardulal100@gmail.com>
2021-12-17 21:31:28 +0545

    Handling the unhandled promise rejection

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,14 @@ dotenv.config({path: 'backend/config/config.env'})
 connectDB();
 
 
-app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+});
+
+// Handle the unhandled promise rejection
+process.on('unhandledRejection', err =>{ console.log(`Error: ${err.message}`);
+console.log('Shutting down the server due to unhandled promise rejection');
+server.close(()=>{
+    process.exit(1)
+})
 })
