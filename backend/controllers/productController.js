1027479a7dd840e0d3ca4a86428295b748629041
Sagar Dulal <sagardulal100@gmail.com>
2021-12-17 23:32:26 +0545

    Pagination added. Result showing per page

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,13 +17,17 @@ exports.newProduct = catchAsyncErrors (async (req,res) =>{
 // Get all products => /api/v1/products
 exports.getProducts =  catchAsyncErrors (async (req,res) =>{
     
+    const resPerPage =4;
+    const productCount = await Product.countDocuments()
+
     const apiFeatures = new APIFeatures(Product.find(), req.query)
-        .search().filter();
+        .search().filter().pagination(resPerPage);
 
     const products = await apiFeatures.query;
     res.status(200).json({
         success:true,
         counts: products.length,
+        productCount,
         products
     })
 })
