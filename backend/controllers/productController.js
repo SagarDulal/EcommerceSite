616d58e0c67e97abf69c68b54956bdf6ca4fdf65
Sagar Dulal <sagardulal100@gmail.com>
2021-12-17 23:11:53 +0545

    Adding search for products and filter functionality to search for specific product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,12 @@
 const Product = require('../models/product')
 const ErrorHandler = require('../utils/errorHandler')
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+const APIFeatures = require('../utils/apiFeatures')
+
 // Create new product = > api/v1/product/new
 
 exports.newProduct = catchAsyncErrors (async (req,res) =>{
+
     const product = await Product.create(req.body);
     res.status(201).json({
         success: true,
@@ -14,7 +17,10 @@ exports.newProduct = catchAsyncErrors (async (req,res) =>{
 // Get all products => /api/v1/products
 exports.getProducts =  catchAsyncErrors (async (req,res) =>{
     
-    const products = await Product.find();
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search().filter();
+
+    const products = await apiFeatures.query;
     res.status(200).json({
         success:true,
         counts: products.length,
---
+++ backend/utils/apiFeatures.js
@@ -0,0 +1,34 @@
+class APIFeatures{
+    constructor(query, queryStr){
+        this.query = query;
+        this.queryStr = queryStr;
+    }
+    search(){
+        const keyword = this.queryStr.keyword ? {
+            name:{
+                $regex: this.queryStr.keyword,
+                $options : 'i'
+            }
+        } : {}
+        this.query = this.query.find({...keyword});
+        return this;
+    }
+    filter(){
+        const queryCopy ={...this.queryStr};
+        console.log(queryCopy)
+        
+        // Removing fields from the query string
+        const removeFields =['keyword','limit','page']
+        removeFields.forEach(elements =>delete queryCopy[elements]);
+        
+        // Advance filter for price , ratings etc
+
+        let queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|llt|lte)\b/g, match=>`$${match}`)
+
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+}
+
+module.exports = APIFeatures
