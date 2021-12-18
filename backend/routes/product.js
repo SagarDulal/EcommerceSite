6c3e7f6e2a3f8fe8cca30aed6edca6fb4e48bc50
Sagar Dulal <sagardulal100@gmail.com>
2021-12-19 00:28:22 +0545

    Protecting the routes from the unauthorized user

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -7,14 +7,25 @@ const {getProducts,
     deleteProduct
  } = require('../controllers/productController')
 
+ const {isAuthenticatedUser} = require('../middlewares/auth')
+
+
+
 // Getting of the products
-router.route('/products').get(getProducts)
+router.route('/products').get(isAuthenticatedUser, getProducts)
 router.route('/product/:id').get(getSingleProduct)
+
+
 
 // Posting new products as well as updating by the admin
 router.route('/admin/products/new').post(newProduct)
 
+
+
 // Updating and delete of the products
 router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct)
+
+
+
 
 module.exports = router
